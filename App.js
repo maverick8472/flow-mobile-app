053c0f9b85150c1eb4c40203582e3d9146d2f120
maverick8472 <bassline_8472@yahoo.com>
2020-04-27 16:31:29 +0200

    initial commit

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      <Text>Flow Habit Tracking app!</Text>
+    </View>
+  );
+}
